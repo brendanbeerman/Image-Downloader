a49aa08131c877e40aa06b218ccc8ea802c116c7
brendanbeerman <brendanbeerman@gmail.com>
2016-03-25 21:28:40 -0300

    Got the image tag finder working, starting javaFX

--- Image_Downloader/src/driver.java
+++ Image_Downloader/src/driver.java
@@ -2,39 +2,41 @@
 TO DO
 
 need to check if a new filename is taken
-find the image tags on a web page, and store them
+gui with javaFX
  
  */
 
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
 
-public class driver {
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class driver extends Application{
 	
-	public static String main_directory = "images";
-	public static String temp_directory = "temp";
-	
-	public static void main (String args[]) throws IOException {
-		//String word = "http://s.4cdn.org/image/fp/atlantis_longcat2.png";
-		String word = "http://=www.4chan.org/";
+	public static void main (String args[]) throws IOException {		
+		checkDirectory("images");
+		checkDirectory("temp");
 		
-		checkDirectory(main_directory);
-		checkDirectory(temp_directory);
-		
+		launch(args);
 		//Scanner sc = new Scanner(System.in);
 		//String address = get_url(sc); 
 		//sc.close();
-		
-		findImageURLs(word);
-		//downloadImage(word);
 	}
 	
 	/*
@@ -50,12 +52,19 @@ public class driver {
 		directory.mkdir();		
 	}
 	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Image Downloader");
+		primaryStage.show();
+	}
+	
 	/*
 	 * Takes in a scanner
 	 * Asks the user for a web site that they want to download images from
 	 * then uses the testConnection function to see if it is valid
 	 * returns a valid URL as a string
 	 */
+
 	public static String getURL (Scanner sc) {
 		System.out.print("\nThe URL needs a 'http://' or 'https://' in the front.");
 		String result;
@@ -96,8 +105,26 @@ public class driver {
 	/* 
 	 * 
 	 */
-	public static void findImageURLs (String address) throws MalformedURLException {
+	public static void findImageURLs (String address) throws IOException {
 		URL url = new URL(address);
+		URLConnection connection = url.openConnection();
+		
+		InputStream input = connection.getInputStream();
+		InputStreamReader input_reader = new InputStreamReader(input);
+		BufferedReader buffer = new BufferedReader(input_reader);
+		
+		HTMLEditorKit editor_kit = new HTMLEditorKit();
+		HTMLDocument document = (HTMLDocument) editor_kit.createDefaultDocument();
+		HTMLEditorKit.Parser parser = new ParserDelegator();
+		HTMLEditorKit.ParserCallback callback = document.getReader(0);
+		parser.parse(buffer, callback, true);
+		
+		for (HTMLDocument.Iterator iterator = document.getIterator(HTML.Tag.IMG); iterator.isValid(); iterator.next()) {
+			AttributeSet attributes = iterator.getAttributes();
+			String image_address = (String) attributes.getAttribute(HTML.Attribute.SRC);
+			System.out.println(image_address.substring(2));
+			downloadImage("http:" +image_address);
+		}
 	}
 	
 	/*
